7c48f1bdd7dfcedb553e907b36972e68dfe17a36
Moralist Festus <46865149+MoralistFestus@users.noreply.github.com>
2020-09-08 03:37:17 +0100

    main.py

--- Day 1/smtp-mailer/main.py
+++ Day 1/smtp-mailer/main.py
@@ -8,19 +8,19 @@ server = smtplib.SMTP('smtp.gmail.com', 465)
 
 server = ehlo()
 
-# server.login('moralistfestus@gmail.com', 'xxxxxx')
+# server.login('yourmail@gmail.com', 'xxxxxx')
 
 # Create a text file to store password
 
 with open('tmp/password.txt', 'r') as f:
 	password = f.read()
-server.login('moralistfestus@gmail.com', password)
+server.login('yourmail@gmail.com', password)
 
 # message body
 
 msg = MIMEMultipart()
 msg['From'] = 'Moralist Festus'
-msg['To'] = 'festusoluwatobi294@gmail.com'
+msg['To'] = 'receiver@gmail.com'
 msg['Subject'] = 'Just a SMTP mail'
 
 with open('tmp/message.txt', 'r') as f:
@@ -42,5 +42,5 @@ p.add_header('Content-Disposition', f'attachment; filename={filename}')
 msg.attach(p)
 
 text = msg.as_string()
-server.sendmail('moralistfestus@gmail.com', 'festusoluwatobi294@gmail.com', text)
+server.sendmail('yourmail@gmail.com', 'receiver@gmail.com', text)
 
