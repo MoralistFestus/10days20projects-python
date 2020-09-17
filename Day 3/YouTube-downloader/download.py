3967dab263c45d2b5e7d9a15f0dbf1f28f402f31
Moralist Festus <46865149+MoralistFestus@users.noreply.github.com>
2020-09-17 15:53:44 +0100

    DOWNLOAD.PY

---
+++ Day 3/YouTube-downloader/download.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+# YouTube('https://youtu.be/9bZkp7q19f0'). streams.first().download()
+
+# Download path
+SAVE_PATH = 'Download/'
+
+# Throw an excemption if some errors occurs
+try:
+	url_link = 'http://youtube.com/watch?v=9bZkp7q19f0'
+	yt = YouTube(url_link)
+	# download 1st stream format
+	stream = yt.streams.first()
+	# download video 📹
+	stream.download(SAVE_PATH)
+	#yt.streams.filter(progressive=True).all()
+	
+	# if an error occurs
+except:
+		print("Failed To Establish Download, Some Error Occurred!")
+		
+		# if no error occurs
+else:
+	print("Successfully Downloaded")
